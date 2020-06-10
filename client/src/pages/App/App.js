e0894c6cf6cd952952680878052e7c68b2298cf2
Paul Susset <paul.susset@gmail.com>
2020-06-10 14:52:31 -0400

    commit

---
+++ client/src/pages/App/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import Home from '../Home';
+import Products from '../Products';
+import ProductDetails from '../ProductDetails';
+import About from '../About';
+
+import Header from '../../components/Header';
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path="/produits">
+          <Products />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/produits/:id">
+          <ProductDetails />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path="/apropos">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const multer = require('multer');
+const PORT = 5000;
+
+const {} = require('./functions');
+
+express()
+  .use((req, res, next) => {
+    res.header(
+      'Access-Control-Allow-Methods',
+      'OPTION, HEAD, GET, PUT, POST, DELETE'
+    );
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+  })
+  .use(morgan('tiny'))
+
+  // endpoints
+  .get('/hello', (req, res) => res.status(200).json('hello world'))
+
+  .listen(PORT, () => console.info(`Listening on port ${PORT}`));
